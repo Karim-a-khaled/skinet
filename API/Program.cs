cab830625d3531c7c08e4139e7ecac87ad9f48d5
karim-khaled <karim-khaled@KARIM-KHALED-WI>
2024-03-17 17:23:18 +0300

    added identity packages, and a part of identity configuration

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddScopedServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
